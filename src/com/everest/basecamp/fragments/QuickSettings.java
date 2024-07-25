xiaomi-sm8450-marble/android_packages_apps_Personalizations

[BETA FEATURE] SystemUI: Implement QS Widgets [2/2]
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/crdroid/settings/fragments/QuickSettings.java
@@ -62,6 +62,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_QS_UI_STYLE  = "qs_tile_ui_style";
     private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
     private static final String KEY_QS_COMPACT_PLAYER  = "qs_compact_media_player_mode";
+    private static final String KEY_QS_WIDGETS_ENABLED  = "qs_widgets_enabled";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
@@ -72,6 +73,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private ListPreference mQsUI;
     private ListPreference mQsPanelStyle;
     private Preference mQsCompactPlayer;
+    private Preference mQsWidgetsPref;
 
     private static ThemeUtils mThemeUtils;
 
@@ -89,6 +91,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
         mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        mQsWidgetsPref = findPreference(KEY_QS_WIDGETS_ENABLED);
+        mQsWidgetsPref.setOnPreferenceChangeListener(this);
         boolean showSlider = LineageSettings.Secure.getIntForUser(resolver,
                 LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
 
@@ -155,6 +159,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             checkQSOverlays(getActivity());
             return true;
         } else if (preference == mQsCompactPlayer) {
+            SystemRestartUtils.showSystemUIRestartDialog(getActivity());
+            return true;
+        } else if (preference == mQsWidgetsPref) {
+            LineageSettings.Secure.putIntForUser(resolver,
+                    LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 0, UserHandle.USER_CURRENT);
             SystemRestartUtils.showSystemUIRestartDialog(getActivity());
             return true;
         }

ATI-dumpster/packages_apps_BaseCamp

src/com/everest/basecamp/fragments/QuickSettings.java
@@ -53,9 +53,11 @@ public class QuickSettings extends SettingsPreferenceFragment
             implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_QS_COMPACT_PLAYER  = "qs_compact_media_player_mode";
+    private static final String KEY_QS_WIDGETS_ENABLED  = "qs_widgets_enabled";
 
     private ListPreference mQuickPulldown;
     private Preference mQsCompactPlayer;
+    private Preference mQsWidgetsPref;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -75,6 +77,8 @@ public class QuickSettings extends SettingsPreferenceFragment
 
         mQsCompactPlayer = (Preference) findPreference(KEY_QS_COMPACT_PLAYER);
         mQsCompactPlayer.setOnPreferenceChangeListener(this);
+        mQsWidgetsPref = findPreference(KEY_QS_WIDGETS_ENABLED);
+        mQsWidgetsPref.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -90,6 +94,11 @@ public class QuickSettings extends SettingsPreferenceFragment
                     mQuickPulldown.getEntries()[index]);
             return true;
         } else if (preference == mQsCompactPlayer) {
+            systemUtils.showSystemUIRestartDialog(getActivity());
+            return true;
+        } else if (preference == mQsWidgetsPref) {
+            Settings.Secure.putIntForUser(resolver,
+                    Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 0, UserHandle.USER_CURRENT);
             systemUtils.showSystemUIRestartDialog(getActivity());
             return true;
         }